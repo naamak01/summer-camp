78830c2f690d7ec49b17e2714e16c06bf98faeb6
Nana Amakye <noamakye@gmail.com>
2024-04-20 01:21:12 -0500

    completed directory page for desktop view

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+import './src/Directory/directory.css';
 import './style.css';
+
+// Targeting animals for picture scrolling (Fade-in)
+// Select all elements with the class 'animating-container'
+const faders = document.querySelectorAll('.animating-container');
+const appearOptions = {
+	threshold: 0,
+	rootMargin: '0px 0px -150px 0px',
+};
+
+// Create an Intersection Observer to add the 'appear'
+// class when an element is in view
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+	entries.forEach((entry) => {
+		// If the element is not intersecting, do nothing
+		if (!entry.isIntersecting) {
+			return;
+		} else {
+			// Add the 'appear' class to the target element
+			entry.target.classList.add('appear');
+			// Stop observing the target element to prevent re-triggering
+			appearOnScroll.unobserve(entry.target);
+		}
+	});
+}, appearOptions);
+
+// Observe each 'animating-container' element with the Intersection Observer
+faders.forEach((fader) => {
+	appearOnScroll.observe(fader);
+});
+
+// // Function to scroll to top when arrow is clicked
+// const btnScrollToTop = document.querySelector('#btnScrollToTop');
+
+// btnScrollToTop.addEventListener('click', function () {
+// 	// Scroll to the top of the page smoothly
+// 	window.scrollTo({
+// 		top: 0,
+// 		left: 0,
+// 		behavior: 'smooth',
+// 	});
+// });
--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,10 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
-				calendar: resolve(__dirname, './src/Calendar/calendar.html'),
-				directory: resolve(__dirname, './src/Directory/directory.html'),
-				registration: resolve(
-					__dirname,
-					'./src/Registration/registration.html'
-				),
-				sports: resolve(__dirname, './src/Sports/sports.html'),
+				sports: resolve(__dirname, './src/Sports/index.html'),
+				directory: resolve(__dirname, './src/Directory/index.html'),
+				calendar: resolve(__dirname, './src/Calendar/index.html'),
+				registration: resolve(__dirname, './src/Registration/index.html'),
 			},
 		},
 	},
