25839d8a26e4ac04aab64e15e32c871203168c5f
Nana Amakye <noamakye@gmail.com>
2024-04-27 17:23:05 -0500

    hiding api

--- firebase-forms.js
+++ firebase-forms.js
@@ -4,15 +4,17 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, push, ref, set } from 'firebase/database';
 
 // Calling firebase configuration to be run for this app
+require('dotenv').config();
+
 const firebaseConfig = {
-	apiKey: 'AIzaSyCkt-oksZL8XUTj4WVcn46llipq5UCuyLc',
-	authDomain: 'summercamp-form.firebaseapp.com',
-	databaseURL: 'https://summercamp-form-default-rtdb.firebaseio.com',
-	projectId: 'summercamp-form',
-	storageBucket: 'summercamp-form.appspot.com',
-	messagingSenderId: '119407229994',
-	appId: '1:119407229994:web:f2c5935e55a07b67919753',
-	measurementId: 'G-1LRXKLN93D',
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
