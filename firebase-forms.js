eb859fa52334f335cedc1f00c291048e9d3f67ac
Nana Amakye <noamakye@gmail.com>
2024-04-27 21:00:53 -0500

    new updates

--- firebase-forms.js
+++ firebase-forms.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 import { getDatabase, push, ref, set } from 'firebase/database';
 
 // Calling firebase configuration to be run for this app
@@ -18,6 +19,16 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+	provider: new ReCaptchaV3Provider('6LcSbckpAAAAALehGUcQZMqvGELTpqYdJ7vbGtP4'),
+
+	// Optional argument. If true, the SDK automatically refreshes App Check
+	// tokens as needed.
+	isTokenAutoRefreshEnabled: true,
+});
 
 // reference database
 const db = getDatabase(app);
